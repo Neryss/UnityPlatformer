146e9b3b9d9fb5d2f368386bd0c6ceba3e6156e5
neryss <kuroysiris@gmail.com>
2020-04-05 23:13:41 +0200

    added a quick camera follow just in case I need it

---
+++ project2D/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float smooth;
+    public bool isFollowing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player && isFollowing)
+        {
+            Vector3 newPos = Vector3.Lerp(transform.position, player.position + offset, smooth * Time.deltaTime);
+            transform.position = newPos;
+        }
+    }
+}
